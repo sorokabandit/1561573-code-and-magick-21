67c4a26e248c80e8a6e0d0651c7f709ca86f0bbd
Danches <danchik995@gmail.com>
2020-10-20 12:32:37 +0300

    работа с сетью

---
+++ js/backend.js
@@ -0,0 +1,54 @@
+'use strict';
+(function () {
+  const saveURL = `https://21.javascript.pages.academy/code-and-magick`;
+  const loadURL = `https://21.javascript.pages.academy/code-and-magick/data`;
+  const StatusCode = {
+    OK: 200
+  };
+  const TIMEOUT_IN_MS = 10000;
+
+
+  window.backend = {
+    save(data, onLoad, onError) {
+      const savexhr = new XMLHttpRequest();
+      savexhr.responseType = `json`;
+
+      savexhr.addEventListener(`load`, function () {
+        onLoad(savexhr.response);
+      });
+      savexhr.addEventListener(`error`, function () {
+        onError(`Произошла ошибка соединения`);
+      });
+
+
+      savexhr.open(`POST`, saveURL);
+      savexhr.send(data);
+    },
+    load(onLoad, onError) {
+      const loadxhr = new XMLHttpRequest();
+      loadxhr.responseType = `json`;
+
+
+      loadxhr.addEventListener(`load`, function () {
+        if (loadxhr.status === StatusCode.OK) {
+          onLoad(loadxhr.response);
+        } else {
+          onError(`Статус ответа: ` + loadxhr.status + ` ` + loadxhr.statusText);
+        }
+      });
+      loadxhr.addEventListener(`error`, function () {
+        onError(`Произошла ошибка соединения`);
+      });
+
+      loadxhr.addEventListener(`timeout`, function () {
+        onError(`Запрос не успел выполниться за ` + loadxhr.timeout + `мс`);
+      });
+
+      loadxhr.timeout = TIMEOUT_IN_MS;
+
+      loadxhr.open(`GET`, loadURL);
+      loadxhr.send();
+    }
+
+  };
+})();
--- js/setup.js
+++ js/setup.js
@@ -8,48 +8,57 @@
   const inputColorEyes = setup.querySelector(`input[name="eyes-color"]`);
   const inputColorCoat = setup.querySelector(`input[name="coat-color"]`);
   const inputColorFireball = setup.querySelector(`input[name="fireball-color"]`);
-  const WIZARD_NAMES = [`Иван `, `Хуан Себастьян `, `Мария `, `Кристоф `, `Виктор `, `Юлия `, `Лопита `, `Вашингтон `];
-  const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топльницкая`, `Нионго`, `Ирвинг`];
   const COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
   const EYES_COLOR = [`red`, `blue`, `yellow`, `green`, `black`];
   const FIRE_BALL = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
-
+  const MAX_SIMILAR_WIZARD_COUNT = 4;
+  const getRandomArrayItem = function (array) {
+    let item = array[Math.floor(Math.random() * array.length)];
+    return item;
+  };
   // волшебники
   const userDialog = document.querySelector(`.setup`);
   userDialog.classList.remove(`hidden`);
 
 
-  document.querySelector(`.setup-similar`).classList.remove(`hidden`);
-
-  function getWizards(n) {
-    let wizards = [];
-    for (let index = 0; index < n; index++) {
-      let wizard = {};
-      wizard.name = getRandomArrayItem(WIZARD_NAMES) + getRandomArrayItem(WIZARD_SURNAMES);
-      wizard.coatColor = getRandomArrayItem(COAT_COLORS);
-      wizard.eyesColor = getRandomArrayItem(EYES_COLOR);
-      wizards.push(wizard);
-    }
-    return wizards;
-  }
-
-  const getRandomArrayItem = function (array) {
-    let item = array[Math.floor(Math.random() * array.length)];
-    return item;
-  };
-  const wizards = getWizards(4);
   const similarListElement = document.querySelector(`.setup-similar-list`);
   const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
     .content
     .querySelector(`.setup-similar-item`);
 
-  for (let i = 0; i < wizards.length; i++) {
-    let wizardElement = similarWizardTemplate.cloneNode(true);
-    wizardElement.querySelector(`.setup-similar-label`).textContent = wizards[i].name;
-    wizardElement.querySelector(`.wizard-coat`).style.fill = wizards[i].coatColor;
-    wizardElement.querySelector(`.wizard-eyes`).style.fill = wizards[i].eyesColor;
-    similarListElement.appendChild(wizardElement);
-  }
+  document.querySelector(`.setup-similar`).classList.remove(`hidden`);
+  const sucsessHandler = function (data) {
+    let wizards = data;
+    for (let i = 0; i < MAX_SIMILAR_WIZARD_COUNT; i++) {
+      let wizardElement = similarWizardTemplate.cloneNode(true);
+      wizardElement.querySelector(`.setup-similar-label`).textContent = wizards[i].name;
+      wizardElement.querySelector(`.wizard-coat`).style.fill = wizards[i].colorCoat;
+      wizardElement.querySelector(`.wizard-eyes`).style.fill = wizards[i].colorEyes;
+      similarListElement.appendChild(wizardElement);
+    }
+  };
+  const errorHandler = function (errorMessage) {
+    const node = document.createElement(`div`);
+    node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
+    node.style.position = `absolute`;
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = `30px`;
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement(`afterbegin`, node);
+  };
+  window.backend.load(sucsessHandler, errorHandler);
+
+  const form = userDialog.querySelector(`.setup-wizard-form`);
+  form.addEventListener(`submit`, function (evt) {
+    window.backend.save(new FormData(form), function () {
+      userDialog.classList.add(`hidden`);
+    });
+    evt.preventDefault();
+  });
+
+
   // изменение цвета глаз и плаща
   const getColorFix = function (element, colors, input) {
     element.addEventListener(`click`, function () {
